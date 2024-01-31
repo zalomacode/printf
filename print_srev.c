15b6714e55eaaf38d6f1e573a32dc8ffb7f8fdb2
zalomacode <zalomaag@gmail.com>
2024-01-31 07:36:33 -0800

    dd

--- print_srev.c
+++ print_srev.c
@@ -21,38 +21,3 @@ int print_revs(va_list args)
 		_putchar(s[i]);
 	return (j);
 }
-
-file: print_str.c
-
-#include "main.h"
-
-/**
- * _strlen - finds the lenght of a string.
- * @s: string
- * Return: integer.
- */
-
-int _strlen(char *s)
-{
-	int c;
-
-	for (c = 0; s[c] != 0; c++)
-		;
-	return (c);
-
-}
-
-/**
- * _strlenc - finds length of a string but for constant characters
- * @s: string
- * Return: integer
- */
-
-int _strlenc(const char *s)
-{
-	int c;
-
-	for (c = 0; s[c] != 0; c++)
-		;
-	return (c);
-}
