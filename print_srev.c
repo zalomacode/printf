1b8c723afd72cd6aa1c2153d0e276c13d67f7705
zalomacode <zalomaag@gmail.com>
2024-01-31 06:38:14 -0800

    printff

---
+++ print_srev.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+ * print_revs - function that prints a str in reverse
+ * @args: type struct va_arg where is allocated printf arguments
+ *
+ * Return: the string
+ */
+int print_revs(va_list args)
+{
+
+	char *s = va_arg(args, char*);
+	int i;
+	int j = 0;
+
+	if (s == NULL)
+		s = "(null)";
+	while (s[j] != '\0')
+		j++;
+	for (i = j - 1; i >= 0; i--)
+		_putchar(s[i]);
+	return (j);
+}
+
+file: print_str.c
+
+#include "main.h"
+
+/**
+ * _strlen - finds the lenght of a string.
+ * @s: string
+ * Return: integer.
+ */
+
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+		;
+	return (c);
+
+}
+
+/**
+ * _strlenc - finds length of a string but for constant characters
+ * @s: string
+ * Return: integer
+ */
+
+int _strlenc(const char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+		;
+	return (c);
+}
