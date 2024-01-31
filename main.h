49f82cf06af0f845be45c5dd4571fdf0143b0917
zalomacode <zalomaag@gmail.com>
2024-01-31 00:29:32 -0800

    header file

--- main.h
+++ main.h
@@ -1,45 +1,37 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
 /**
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
+ * Struct to store conversion specifier and corresponding function.
  */
-int _printf(const char * const format, ...)
-{
-	convert_match m[] = {
-		{"%s", printf_string}, {"%c", printf_char},
-		{"%%", printf_37},
-		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
-		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
-		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
-		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
-	};
+typedef struct {
+    const char *id;
+    int (*f)(va_list);
+} convert_match;
 
-	va_list args;
-	int i = 0, j, len = 0;
+/**
+ * Function prototypes for printf-related functions.
+ */
+int printf_string(va_list args);
+int printf_char(va_list args);
+int printf_37(va_list args);
+int printf_int(va_list args);
+int printf_dec(va_list args);
+int printf_srev(va_list args);
+int printf_rot13(va_list args);
+int printf_bin(va_list args);
+int printf_unsigned(va_list args);
+int printf_oct(va_list args);
+int printf_hex(va_list args);
+int printf_HEX(va_list args);
+int printf_exclusive_string(va_list args);
+int printf_pointer(va_list args);
 
-	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
+/**
+ * Your _printf function prototype.
+ */
+int _printf(const char * const format, ...);
 
-Here:
-	while (format[i] != '\0')
-	{
-		j = 13;
-		while (j >= 0)
-		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
-			{
-				len += m[j].f(args);
-				i = i + 2;
-				goto Here;
-			}
-			j--;
-		}
-		_putchar(format[i]);
-		len++;
-		i++;
-	}
-	va_end(args);
-	return (len);
-}
+#endif /* MAIN_H */
