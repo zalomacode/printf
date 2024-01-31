6a09a26a3fa7c9ab849f482b905ad521029eb950
zalomacode <zalomaag@gmail.com>
2024-01-31 07:29:40 -0800

    vv

--- main.h
+++ main.h
@@ -25,7 +25,6 @@ int *_strcpy(char *dest, char *src);
 int print_pointer(va_list val);
 int print_unsigned(va_list args);
 int print_hex_extra(unsigned long int num);
-int _strlenc(const char *s);
 int print_HEX_extra(unsigned int num);
 int print_exc_string(va_list val);
 int print_HEX(va_list val);
