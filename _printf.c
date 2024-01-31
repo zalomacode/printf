c7c4943685c83d457a4079be2801d49437717c11
zalomacode <zalomaag@gmail.com>
2024-01-31 01:33:48 -0800

    FIRST TASK

--- _printf.c
+++ _printf.c
@@ -1,45 +1,136 @@
 #include "main.h"
-/**
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
- */
-int _printf(const char * const format, ...)
-{
-	convert_match m[] = {
-		{"%s", printf_string}, {"%c", printf_char},
-		{"%%", printf_37},
-		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
-		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
-		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
-		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
-	};
 
+/**
+ * _printf - our magical custom printf function,
+ *  handles (d, i, b, r, R) specifiers
+ * @format: the inserted format
+ * Return: count
+ */
+
+int _printf(const char *format, ...)
+{
+	int count = 0;
 	va_list args;
-	int i = 0, j, len = 0;
 
 	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-
-Here:
-	while (format[i] != '\0')
+	while (*format != '\0')
 	{
-		j = 13;
-		while (j >= 0)
+		if (*format == '%')
 		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			switch (*(++format))
 			{
-				len += m[j].f(args);
-				i = i + 2;
-				goto Here;
+				case 'd':
+				case 'i':
+					count += I_handle_d_specifier(va_arg(args, int));
+					break;
+				case 'b':
+					count += I_handle_b_specifier(va_arg(args, int));
+					break;
+				case 'R':
+					count += I_handle_R_specifier(va_arg(args, char *));
+					break;
+				case 'r':
+					count += I_handle_r_specifier(va_arg(args, char *));
+					break;
+				default:
+					count += I_handle_chars_specifiers(args, format);
+					break;
 			}
-			j--;
 		}
-		_putchar(format[i]);
-		len++;
-		i++;
+		else
+		{
+			count += _putchar(*format);
+		}
+		format++;
 	}
 	va_end(args);
-	return (len);
+	return (count);
+}
+
+/**
+ * I_handle_chars_specifiers - handles (c, s, %, S) specifers
+ * @args: agrs
+ * @format: the inserted format
+ * Return: count
+ */
+int I_handle_chars_specifiers(va_list args, const char *format)
+{
+	int count = 0;
+
+	switch (*format)
+	{
+		case 'c':
+			count += _putchar(va_arg(args, int));
+			break;
+		case 's':
+			count += I_handle_s_specifier(va_arg(args, char *));
+			break;
+		case '%':
+			count += _putchar(*format);
+			break;
+		case 'S':
+			count += I_handle_S_specifier(va_arg(args, char *));
+			break;
+		default:
+			count += I_handle_numbers_specifiers(args, format);
+			break;
+	}
+	return (count);
+}
+
+/**
+ * I_handle_numbers_specifiers - handles (u, o, x, X) specifiers
+ * @args: agrs
+ * @format: the inserted format
+ * Return: count
+ */
+int I_handle_numbers_specifiers(va_list args, const char *format)
+{
+	int count = 0;
+
+	switch (*format)
+	{
+		case 'u':
+			count += handler_u_specifier(va_arg(args, unsigned int));
+			break;
+		case 'o':
+			count += handler_o_specifier(va_arg(args, unsigned int));
+			break;
+		case 'x':
+			count += handler_x_specifier(va_arg(args, unsigned int));
+			break;
+		case 'X':
+			count += handler_X_specifier(va_arg(args, unsigned int));
+			break;
+		default:
+		count += I_handle_pointer(args, format);
+			break;
+	}
+
+	return (count);
+}
+/**
+ * I_handle_pointer - handle (p) specifier
+ * @args: agrs
+ * @format: the inserted format
+ * Return: count
+ */
+int I_handle_pointer(va_list args, const char *format)
+{
+	int count = 0;
+
+	switch (*format)
+	{
+				case 'p':
+			count += handler_p_specifier(va_arg(args, void *));
+			break;
+
+		default:
+			count += _putchar('%');
+			if (*format)
+				count += _putchar(*format);
+			break;
+	}
+
+	return (count);
 }
--- main.h
+++ main.h
@@ -1,49 +1,37 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h>
 #include <stdarg.h>
+#include <stdio.h>
 #include <limits.h>
+#include <stdlib.h>
 #include <unistd.h>
 
-
-
-/**
- * struct format - match the conversion specifiers for printf
- * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
- * @f: type pointer to function for the conversion specifier
- *
- */
-
-typedef struct format
-{
-	char *id;
-	int (*f)();
-} convert_match;
-
-int printf_pointer(va_list val);
-int printf_hex_aux(unsigned long int num);
-int printf_HEX_aux(unsigned int num);
-int printf_exclusive_string(va_list val);
-int printf_HEX(va_list val);
-int printf_hex(va_list val);
-int printf_oct(va_list val);
-int printf_unsigned(va_list args);
-int printf_bin(va_list val);
-int printf_srev(va_list args);
-int printf_rot13(va_list args);
-int printf_int(va_list args);
-int printf_dec(va_list args);
-int _strlen(char *s);
-int *_strcpy(char *dest, char *src);
-int _strlenc(const char *s);
-int rev_string(char *s);
-int _strlenc(const char *s);
-int printf_37(void);
-int printf_char(va_list val);
-int printf_string(va_list val);
 int _putchar(char c);
+int _puts(const char *s);
 int _printf(const char *format, ...);
+int I_handle_chars_specifiers(va_list args, const char *format);
+int I_handle_numbers_specifiers(va_list args, const char *format);
+int I_handle_pointer(va_list args, const char *format);
 
-#endif
+int I_handle_d_specifier(int n);
+int I_handle_b_specifier(int n);
+int I_handle_i_specifier(int n);
+int I_handle_R_specifier(char *str);
+int I_handle_r_specifier(char *str);
+
+int handler_u_specifier(unsigned int n);
+int handler_o_specifier(unsigned int n);
+int handler_x_specifier(unsigned int n);
+int handler_X_specifier(unsigned int n);
+int handler_p_specifier(void *p);
+
+
+
+int I_handle_S_specifier(char *str);
+int I_handle_s_specifier(char *str);
+int print_integer(int n);
+
+
+#endif /* MAIN_H */
