74984cc0dc62f93d2e04ef26160e72168e56bc76
zalomacode <zalomaag@gmail.com>
2024-01-31 07:07:50 -0800

    new printf

--- print_str.c
+++ print_str.c
@@ -30,35 +30,3 @@ int _strlenc(const char *s)
 		;
 	return (c);
 }
-
-file: print_string.c
-
-#include "main.h"
-/**
- * print_s - print a string.
- * @val: argumen t.
- * Return: the length of the string.
- */
-
-int print_s(va_list val)
-{
-	char *s;
-	int i, len;
-
-	s = va_arg(val, char *);
-	if (s == NULL)
-	{
-		s = "(null)";
-		len = _strlen(s);
-		for (i = 0; i < len; i++)
-			_putchar(s[i]);
-		return (len);
-	}
-	else
-	{
-		len = _strlen(s);
-		for (i = 0; i < len; i++)
-			_putchar(s[i]);
-		return (len);
-	}
-}
